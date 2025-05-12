242674050f34f60443e2564655004a62aa1b7424
rahultsbi <rahulkumar@tsbi.in>
2025-05-12 18:13:35 +0530

    'add191best'

--- src/components/Shared/ProtectedRoute.jsx
+++ src/components/Shared/ProtectedRoute.jsx
@@ -1,39 +1,4 @@
-// // // import React from 'react';
-// // // import { Route, Redirect } from 'react-router-dom';
-// // // import { isAuthenticated } from '../../services/auth';
 
-// // // function ProtectedRoute({ component: Component, ...rest }) {
-// // //   return (
-// // //     <Route
-// // //       {...rest}
-// // //       render={(props) =>
-// // //         isAuthenticated() ? (
-// // //           <Component {...props} />
-// // //         ) : (
-// // //           <Redirect to="/admin/login" />
-// // //         )
-// // //       }
-// // //     />
-// // //   );
-// // // }
-
-// // // export default ProtectedRoute;
-// // import { Navigate, Outlet } from 'react-router-dom';
-// // import { isAuthenticated } from '../../services/auth';
-
-// // function ProtectedRoute() {
-// //   return isAuthenticated() ? <Outlet /> : <Navigate to="/admin/login" />;
-// // }
-
-// // export default ProtectedRoute;
-// import { Navigate } from 'react-router-dom';
-// import { isAuthenticated } from '../../services/auth';
-
-// function ProtectedRoute({ children }) {
-//   return isAuthenticated() ? children : <Navigate to="/admin/login" replace />;
-// }
-
-// export default ProtectedRoute;
 import { Navigate } from 'react-router-dom';
 import { isAuthenticated } from '../../services/auth';
 
--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,3 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
